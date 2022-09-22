95bd1043f4a937d9b952fe760b94af0537fc78f5
Filipe Rodrigues <lipe_rsilva@hotmail.com>
2022-09-22 00:03:03 -0300

    feat: inicio landing page

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 55.94%, #E1D55D 33.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
+      },
+    },
   },
   plugins: [],
 }
